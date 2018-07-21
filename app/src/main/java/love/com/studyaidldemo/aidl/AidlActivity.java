d18975e006e2569378a0a62d8a2b21a23ce2a101
weimingchuan <1107963848@qq.com>
2018-07-21 20:01:49 +0800

    可运行的版本

--- app/src/main/java/love/com/studyaidldemo/aidl/AidlActivity.java
+++ app/src/main/java/love/com/studyaidldemo/aidl/AidlActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import love.com.studyaidldemo.R;
 
@@ -14,12 +16,35 @@ import static love.com.studyaidldemo.ConstantBean.TAG;
 
 public class AidlActivity extends AppCompatActivity {
 
+    private RemotePersonAidl remotePersonAidl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aidl);
 
         bindMessengerService();
+
+        findViewById(R.id.sendToServer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PersonBean personBean = new PersonBean();
+                personBean.setAge(12);
+                personBean.setName("zhangsan");
+                try {
+                    remotePersonAidl.addPerson(personBean);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Log.i(TAG, "aidl service receive PersonBean:" + remotePersonAidl.getPerson().size());
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
 
@@ -31,13 +56,14 @@ public class AidlActivity extends AppCompatActivity {
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
                 Log.i(TAG, "messenger service onServiceConnected");
+                remotePersonAidl = RemotePersonAidl.Stub.asInterface(service);
 
             }
 
             @Override
             public void onServiceDisconnected(ComponentName name) {
                 Log.i(TAG, "messenger service onServiceDisconnected");
-
+                remotePersonAidl = null;
 
             }
         }, BIND_AUTO_CREATE);
--- app/src/main/java/love/com/studyaidldemo/aidl/RemoteAidlService.java
+++ app/src/main/java/love/com/studyaidldemo/aidl/RemoteAidlService.java
@@ -3,7 +3,14 @@ package love.com.studyaidldemo.aidl;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static love.com.studyaidldemo.ConstantBean.TAG;
 
 /**
  * Created by love on 2018/7/21.
@@ -11,16 +18,32 @@ import android.support.annotation.Nullable;
 
 public class RemoteAidlService extends Service {
 
+    private IBinder iBinder = new RemotePersonAidl.Stub() {
+
+        @Override
+        public void addPerson(PersonBean book) throws RemoteException {
+            personBeanList.add(book);
+            Log.i(TAG, "aidl service receive PersonBean:" + book.getName() + " : " + book.getAge());
+        }
+
+        @Override
+        public List<PersonBean> getPerson() throws RemoteException {
+
+            return personBeanList;
+        }
+    };
+    private List<PersonBean> personBeanList;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        personBeanList = new ArrayList<>();
 
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-
-        return null;
+        return iBinder;
     }
 }
